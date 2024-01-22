cd7f95f3efc37de4cf9888455e51604dc6925181
sasayomi <90307002+sasayomi@users.noreply.github.com>
2024-01-23 00:01:13 +0900

    Update SLT5006_Serial&LCD1602.ino

--- SLT5006_Serial&LCD1602.ino
+++ SLT5006_Serial&LCD1602.ino
@@ -31,72 +31,85 @@ int flag;
 int size;
 
 void writecom(int size, int value[]){//センサ送信
-for (int i=0;i<size;i++){
- mySerial.write(value[i]);
+  for (int i=0;i<size;i++){
+    mySerial.write(value[i]);
+  }
+
+  while (mySerial.available()>0){
+    char t = mySerial.read();
+  }
+/*デバッグ用
+  Serial.print("From Arduino:");Serial.print(" ");
+  for (int i=0;i<size;i++){
+    Serial.print(value[i],HEX);Serial.print(" "); 
+  }
+  Serial.print(flag);  Serial.println("send");
+*/
 }
 
-while (mySerial.available()>0){
- char t = mySerial.read();
-}
-  Serial.print("From Arduino:");Serial.print(" ");
-for (int i=0;i<size;i++){
- Serial.print(value[i],HEX);Serial.print(" "); 
-}
-Serial.print(flag);  Serial.println("send");
-}
 
 void receive(int valn){//センサ受信
-int val[valn]={0};
-if(mySerial.available()>0){
-for (int i=0; i<valn;i++){
-  val[i]=mySerial.read();
-}
-}
-while (mySerial.available()>0){
- char t = mySerial.read();
-}
-Serial.print(flag);Serial.print("fromsensor");
-for (int i=0;i<valn;i++){
- Serial.print(val[i],HEX);;Serial.print(" "); 
-}
-Serial.print(flag);Serial.println("receive");
-if((val[1]==8)&&(val[3]==1)){
-  flag=1;
- Serial.println("read/measured");
-  for (int i=0; i<valn;i++){
-    Serial.print(val[i],HEX);Serial.print(" ");
+  int val[valn]={0};
+  if(mySerial.available()>0){
+    for (int i=0; i<valn;i++){
+      val[i]=mySerial.read();
+    }
+  }
+  while (mySerial.available()>0){
+    char t = mySerial.read();
+  }
+/*デバッグ用
+  Serial.print(flag);Serial.print("fromsensor");
+  for (int i=0;i<valn;i++){
+    Serial.print(val[i],HEX);;Serial.print(" "); 
+  }
+  Serial.print(flag);Serial.println("receive");
+  */
+  if((val[1]==8)&&(val[3]==1)){
+    flag=1;
+    /*デバッグ用
+    Serial.println("read/measured");
+    for (int i=0; i<valn;i++){
+      Serial.print(val[i],HEX);Serial.print(" ");
      }
   }else{
-      Serial.println("readonly");
-         }
+  Serial.println("readonly");
+  */
+  }
 }
 
 void receive2(int valn){
   valn=21;
-int val[valn]={0};
-if(mySerial.available()>0){
-for (int i=0; i<valn;i++){
-  val[i]=mySerial.read();
-}
-}
-while (mySerial.available()>0){
- char t = mySerial.read();
-}
-Serial.print(flag);Serial.print("fromsensor");
-for (int i=0;i<valn;i++){
- Serial.print(val[i],HEX);;Serial.print(" "); 
-}
-Serial.print(flag);Serial.println("receive");
+  int val[valn]={0};
+  if(mySerial.available()>0){
+    for (int i=0; i<valn;i++){
+      val[i]=mySerial.read();
+    }
+  }
+  while (mySerial.available()>0){
+    char t = mySerial.read();
+  }
+/*デバッグ用
+  Serial.print(flag);Serial.print("fromsensor");
+  for (int i=0;i<valn;i++){
+    Serial.print(val[i],HEX);;Serial.print(" "); 
+  }
+  Serial.print(flag);Serial.print("receive");
+  */
+  Serial.println();
 
-if((val[1]==8)&&(val[3]==1)){
-  flag=1;
- Serial.println("read/measured");
-  for (int i=0; i<valn;i++){
-    Serial.print(val[i],HEX);Serial.print(" ");
+  if((val[1]==8)&&(val[3]==1)){
+    flag=1;
+    /*デバッグ用
+    Serial.println("read/measured");
+    for (int i=0; i<valn;i++){
+      Serial.print(val[i],HEX);Serial.print(" ");
      }
   }else{
       Serial.println("readonly");
-         }
+      */
+  }
+
 float temp=0;
 float bulk=0;
 float VWC=0;
@@ -114,28 +127,37 @@ display(flag,temp,bulk,VWC,pore,VWCR,VWCC);
 }
 
 void display(int flag2,float temp2,float bulk2,float VWC2,float pore2,float VWCR2,float VWCC2){
- if(flag2==1) {
-  Serial.print(flag);Serial.println("display");
-  Serial.print(",");Serial.print(temp2);
-  Serial.print(",");Serial.print(bulk2);
-  Serial.print(",");Serial.print(VWC2);
-  Serial.print(",");Serial.print(pore2);Serial.println(",");
+  if(flag2==1) {
+ //Serial.print(flag);Serial.println("display");//デバッグ用
+  Serial.println("=============================");
+  Serial.print("Temprature [degC],");
+  Serial.print("EC BULK [dS/m],");
+  Serial.print("VWC [%],");
+  Serial.print("EC PORE [dS/m]"); 
+  Serial.print("VWC-ROCK [%],");
+  Serial.println("VWC-COCO [%],");
+  Serial.print(temp2); Serial.print(",");
+  Serial.print(bulk2); Serial.print(","); 
+  Serial.print(VWC2); Serial.print(","); 
+  Serial.print(pore2); Serial.print(""); 
+  Serial.print(VWCR2); Serial.print(","); 
+  Serial.print(VWCC2); Serial.println(","); 
 
- lcd.clear();
- lcd.setCursor(0, 0);
- lcd.print(temp2);lcd.print("deg");
- lcd.setCursor(8, 0);
- lcd.print(bulk2);lcd.print("dS/m");
- lcd.setCursor(0, 1);
- lcd.print(VWC2);lcd.print("%");
- lcd.setCursor(8, 1);
- lcd.print(pore2);lcd.print("dS/m");
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(temp2);lcd.print("deg");
+  lcd.setCursor(8, 0);
+  lcd.print(bulk2);lcd.print("dS/m");
+  lcd.setCursor(0, 1);
+  lcd.print(VWC2);lcd.print("%");
+  lcd.setCursor(8, 1);
+  lcd.print(pore2);lcd.print("dS/m");
  }
 }
 
 void program(){
   //測定開始
-  Serial.print("Start sensor");
+ // Serial.print("Start sensor");デバッグ用
   int  value[]={0X02,0X07,0X01,0X01,0X0D,0X70};
   writecom(6,value);
   delay(100);
@@ -144,7 +166,7 @@ void program(){
  //測定完了ステータス読み出し
   flag=0;
  while(flag==0){
-  Serial.print("Read status");
+//  Serial.print("Read status");デバッグ用
   int value1[]={0X01,0X08,0X01,0X00,0XE6,0X00};
   writecom(5,value1);
   delay(100);
@@ -166,12 +188,19 @@ void setup() {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Soil Sensor");
-
+  Serial.println("Soil sensor");
+  Serial.println("=============================");
+  Serial.print("Temprature [degC],");
+  Serial.print("EC BULK [dS/m],");
+  Serial.print("VWC [%],");
+  Serial.print("EC PORE [dS/m]"); 
+  Serial.print("VWC-ROCK [%],");
+  Serial.println("VWC-COCO [%],");
 }
 
 
 void loop() {
-program();
-int interval=3000;
-delay(interval);//測定間隔(ms)
+  program();
+  int interval=3000;
+  delay(interval);//測定間隔(ms)
 }
